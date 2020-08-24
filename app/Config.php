9ead2f8c570810be221500c42d8c83dc32edbc9b
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-08-24 22:38:30 +0900

    config of heroku postgres added

--- app/Config.php
+++ app/Config.php
@@ -23,18 +23,20 @@ class Config
   ];
   
   // DB接続設定
-  /*
+  $url = parse_url(getenv('DATABASE_URL'));
+  $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+  
   const DB_CONFIGS = [
-    'dsn' => 'pgsql:host=ec2-34-192-122-0.compute-1.amazonaws.com;dbname=detc6rc862nru9',
-    'user' => 'swptknnaafikms',
-    'password' => '5ba118530fe50eac1b5a15f1daaa398c5a500c86c6aefaeecc1445c2c59e5dca',
+    'dsn' => $dsn,
+    'user' => $url['user'],
+    'password' => $url['pass'],
     'options' => [],
   ];
-  */
-  const DB_CONFIGS = [
+
+/*  const DB_CONFIGS = [
     'dsn' => 'sqlite:../sqlite/blog',
     'user' => '',
     'password' => '',
     'options' => [],
-  ];
+  ];*/
 }

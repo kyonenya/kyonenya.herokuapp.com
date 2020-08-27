d53fbf209ff35f90f586ea3541ac155bcf7d3278
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-08-27 18:20:30 +0900

    debugcode added

--- web/index.php
+++ web/index.php
@@ -5,12 +5,12 @@
  * ブラウザからの全てのアクセスをここに集める。
  */
 require '../bootstrap.php';
-// $_ENV['PHP_ENV'] = 'heroku';
-$app = new App();
-$app->run();
 
 // デバッグ用
 echo '<pre>';
 var_dump($_ENV['DATABASE_URL']);
 var_dump(getenv('DATABASE_URL'));
 echo '</pre>';
+
+$app = new App();
+$app->run();

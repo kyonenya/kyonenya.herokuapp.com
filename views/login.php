66fe3eece119369d5dd7ff90ce1aaa7455ff1ca2
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2020-09-10 12:50:56 +0900

    renamed

--- views/login.php
+++
@@ -1,6 +0,0 @@
-  <h2 class="el_heading1">管理者ログイン</h2>
-
-  <form action="<?= $baseUrl ?>/admin/auth" method="post" class="bl_editor">
-    <input type="text" name="password" placeholder="password" />
-    <button>ログイン</button>
-  </form>
